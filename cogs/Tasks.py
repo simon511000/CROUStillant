dca7b6b5fc5aba3e207d7157a4f41d2fca5fc1a0
Polsulpicien <70817001+Polsulpicien@users.noreply.github.com>
2022-11-27 18:33:55 +0100

    Update Tasks.py

--- cogs/Tasks.py
+++ cogs/Tasks.py
@@ -11,8 +11,7 @@ from datetime import time
 
 
 class Tasks(commands.Cog):
-    time_one = time(hour=1, minute=0, tzinfo=pytz.timezone("Europe/Paris"))
-    time_two = time(hour=9, minute=15, tzinfo=pytz.timezone("Europe/Paris"))
+    time = time(hour=1, minute=0, tzinfo=pytz.timezone("Europe/Paris"))
 
     def __init__(self, client):
         self.client = client
@@ -28,7 +27,7 @@ class Tasks(commands.Cog):
         self.daily_task.cancel()
 
 
-    @tasks.loop(time=time_to_execute)
+    @tasks.loop(time=time)
     async def daily_task(self):
         await run_task(self.client)
 
